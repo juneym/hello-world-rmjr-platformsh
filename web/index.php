d7bece640648378c0ae4df046164a2ba44990410
Raul Martinez Jr <juneym@gmail.com>
2018-07-07 00:31:10 +0800

    Added fully working Slimframework code

--- web/index.php
+++ web/index.php
@@ -1,3 +1,25 @@
 <?php
 
-phpinfo();
+require_once __DIR__ . "/../vendor/autoload.php";
+
+// Create and configure Slim app
+$config = ['settings' => [
+    'addContentLengthHeader' => false,
+]];
+$app = new \Slim\App($config);
+
+// Define app routes
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->write("Hello " . $args['name']);
+});
+
+$app->get("/", function($request, $response, $args) {
+    return $response->write("
+    Welcome! This is a test app for evaluating Platform.sh
+    
+    Hit the URL path: \"/hello/Jack\" and see what happens. 
+    ");
+});
+
+// Run app
+$app->run();
